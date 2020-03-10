4fcd5635416d55b0eff481012fc97fa5f4e40bb1
Moniiiii <39875081+Moniiiii@users.noreply.github.com>
2020-03-10 11:36:06 +0200

    Add files via upload

---
+++ Variables/Forms/Locators/CategorySearchLocators.py
@@ -0,0 +1,3 @@
+category_women = "xpath = //a[@title='Women']"
+blouses = "xpath = //a[@title='Blouses']"
+category_name = "xpath = //span[@class='cat-name']"
---
+++ Variables/Forms/Locators/SearchLocators.py
@@ -0,0 +1,3 @@
+type_in_search_field = "xpath = //input [@id='search_query_top']"
+search_icon = "xpath = //button[@name='submit_search']"
+search_result = "xpath = //span[@class='lighter']"
